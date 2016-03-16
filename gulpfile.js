a31baece727e573ca2e70b1f0da4e532083777ac
Blake Scott <blakerscott@gmail.com>
2016-03-16 10:34:43 -0700

    add browser-sync dependency to npm manifest file and add basic html for project

--- gulpfile.js
+++ gulpfile.js
@@ -54,3 +54,17 @@ gulp.task('jshint', function(){
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
+
+//Browsersync task
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(['*.html'], ['htmlBuild']);
+});
