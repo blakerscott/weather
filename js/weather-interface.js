d16972e6b9167abe72df7fba661bdb1c767b9c2c
Blake Scott <blakerscott@gmail.com>
2016-03-16 13:47:07 -0700

    update manifest files with /weather

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -5,5 +5,8 @@ $(document).ready(function() {
     var city = $('#location').val();
     $('#location').val("");
     $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+    });
   });
 });
