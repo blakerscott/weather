843978233d39c7e4f24c30e5bc89bacd5c80bfdf
Blake Scott <blakerscott@gmail.com>
2016-03-16 16:04:39 -0700

    update weather-interface to display the humidiy of the inputted city

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,16 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-sadfkl/sadfnow39r9u0rhiow4anklfdnms/v;_-=09
+
+
+var apiKey = "ea4a1c467b5ed0a0b65aaace5c70f5d2";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
 
 },{}]},{},[1]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,1 +1,13 @@
 
+
+var apiKey = "ea4a1c467b5ed0a0b65aaace5c70f5d2";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
